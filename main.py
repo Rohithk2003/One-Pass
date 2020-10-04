4893f78a58c2e892cd78c0980517ffd0b2b723d4
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2020-10-04 23:05:36 +0530

    testing

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ jump = pygame.image.load('jump.png')
 low = [pygame.image.load('low1.png'), pygame.image.load('low2.png')]
 run = [pygame.image.load('run1.png'), pygame.image.load('run2.png')]
 back = pygame.image.load('back.png')
+#for movement of the player
 running = False
 jumping = False
 low1 = False
+#coordinates of player
 x = 50
 y = 425
+
 walkcount = 0
+
 # color
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -20,7 +24,6 @@ red = (255, 0, 0)
 blue = (0,255,0)
 
 # for the movement and display of the game and display player images as sprite
-
 
 def redraw():
     global walkcount
@@ -37,30 +40,31 @@ def redraw():
 
     pygame.display.update()
 
+def gameloop():
+    while True:
 
-while True:
-
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT]:
-        x += 0.5
-        running = True
-        jumping = False
-        low1 = False
-    elif keys[pygame.K_SPACE]:
-        jumping = True
-        running = False
-        low1 = False
-    elif keys[pygame.K_e]:
-        low1 = True
-        jumping = False
-        running = False
-    else:
-        running = False
-        jumping = False
-        low1 = False
-        walkcount = 0
-    redraw()
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            x += 0.5
+            running = True
+            jumping = False
+            low1 = False
+        elif keys[pygame.K_SPACE]:
+            jumping = True
+            running = False
+            low1 = False
+        elif keys[pygame.K_e]:
+            low1 = True
+            jumping = False
+            running = False
+        else:
+            running = False
+            jumping = False
+            low1 = False
+            walkcount = 0
+        redraw()
+gameloop()
