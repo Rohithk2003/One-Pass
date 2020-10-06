41fcc39e5c267e9205107cd3891356705e8a40e2
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2020-10-06 09:40:34 +0530

    added fighting, jumping

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ from pygame.locals import *
 import os
 import sys
 import math
+import time
 
 pygame.init()
 clock = pygame.time.Clock()
 # for display
-win = pygame.display.set_mode((800, 600))
-idle = [pygame.image.load('idle0.png'), pygame.image.load('idle1.png'), pygame.image.load('idle2.png'), pygame.image.load(
-    'idle3.png'), pygame.image.load('idle0.png'), pygame.image.load('idle1.png'), pygame.image.load('idle2.png'), pygame.image.load('idle3.png')]
-low = [pygame.image.load('slide0.png'), pygame.image.load('slide1.png'), pygame.image.load(
-    'slide0.png'), pygame.image.load('slide1.png'), pygame.image.load('slide0.png'), pygame.image.load('slide1.png')]
-run = [pygame.image.load('run1.png'), pygame.image.load('run2.png'), pygame.image.load(
-    'run3.png'), pygame.image.load('run4.png'), pygame.image.load('run5.png'), pygame.image.load('run5.png')]
+win = pygame.display.set_mode((1400, 447))
+idle = [pygame.image.load(os.path.join(
+    'data', 'idle' + str(x)+'.png')) for x in range(0, 10)]
+low = [pygame.image.load(os.path.join(
+    'data', 'slide' + str(x)+'.png')) for x in range(0, 10)]
+run = [pygame.image.load(os.path.join('data', 'run' + str(x)+'.png'))
+       for x in range(0, 10)]
 back = pygame.image.load('bg.png')
-jump = [pygame.image.load('jump0.png'), pygame.image.load('jump3.png'), pygame.image.load(
-    'jump10.png'), pygame.image.load('jump11.png'), pygame.image.load('jump12.png'), pygame.image.load('jump13.png')]
+jump = [pygame.image.load(os.path.join(
+    'data', 'jump' + str(x)+'.png')) for x in range(0, 10)]
+fight = [pygame.image.load(os.path.join(
+    'data', 'attack' + str(x)+'.png')) for x in range(0, 10)]
 
-bgX = 0 
+bgX = 0
 bgX2 = back.get_width()
+
 
 class player(object):
     def __init__(self, player_x, player_y):
@@ -31,18 +35,28 @@ class player(object):
         self.jumpcount = 10
         self.walkcount = 0
         self.idle1 = False
-    def draw(self, win):
-        if self.walkcount + 1 >= 18:
-            self.walkcount = 0
+        self.fighting = False
+        self.testing_count = 0
 
+    def draw(self, win):
+        if self.walkcount + 1 >= 27:
+            self.walkcount = 0
         if self.running:
             self.walkcount += 1
             win.blit(run[self.walkcount // 3], (self.x, self.y))
         elif self.low1:
             win.blit(low[self.walkcount // 3], (self.x, self.y))
             self.walkcount += 1
+
         elif self.idle1:
             win.blit(idle[int(self.walkcount // 3)], (self.x, self.y))
+
+    def testing(self, win):
+        if self.testing_count > 27:
+            self.testing_count = 0
+        if self.fighting:
+            win.blit(fight[(self.testing_count // 3)], (self.x, self.y))
+            self.testing_count += 1
 
 
 # color
@@ -54,7 +68,7 @@ green = (0, 0, 255)
 
 # for the movement and display of the game and display player images as sprite
 
-man = player(100, 500)
+man = player(100, 320)
 
 
 def redraw():
@@ -63,50 +77,71 @@ def redraw():
     win.blit(back, (bgX2, 0))
 
     man.draw(win)
+    man.testing(win)
     pygame.display.update()
 
-pygame.time.set_timer(USEREVENT+1, 500) # Sets the timer for 0.5 seconds
-fps = 60
+
+pygame.time.set_timer(USEREVENT + 1, 500)  # Sets the timer for 0.5 seconds
+fps = 27
+
+
 def gameloop():
-    global fps 
+    global fps
     global bgX
     global bgX2
+    a = False
     while True:
         redraw()
-        bgX -= 2
-        bgX2 -= 2
+        if a:
+            bgX -= 2
+            bgX2 -= 2
         if bgX < back.get_width() * -1:  # If our bg is at the -width then reset its position
             bgX = back.get_width()
-    
+
         if bgX2 < back.get_width() * -1:
             bgX2 = back.get_width()
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if e.type == USEREVENT+1:
+            if e.type == USEREVENT + 1:
                 fps += 1
 
         keys = pygame.key.get_pressed()
+        man.running = True
         if keys[pygame.K_RIGHT]:
             man.x += 0.1
-            man.running = True
+            a = True
             man.low1 = False
+            man.fighting = False
             man.idle1 = False
+
             if keys[pygame.K_c]:
-                            man.x += 0.1
-                            man.low1 = True
-                            man.running = False
-                            man.idle1 = False 
-        elif keys[pygame.K_c] :
+                man.x += 0.1
+                man.low1 = True
+                man.running = False
+                man.fighting = False
+                man.idle1 = False
+
+        elif keys[pygame.K_c]:
             man.x += 0.1
+            a = False
             man.low1 = True
             man.running = False
-            man.idle1 = False 
+            man.fighting = False
+            man.idle1 = False
+        elif keys[pygame.K_p]:
+            man.fighting = True
+            man.low1 = False
+            man.running = False
+            man.idle1 = False
         else:
+            a = False
+            man.fighting = False
             man.idle1 = True
             man.running = False
             man.low1 = False
+
             man.walkcount = 0
         if not(man.isjump) and man.low1 == False:
             if keys[pygame.K_SPACE]:
@@ -117,14 +152,13 @@ def gameloop():
                     var = -1
                 else:
                     var = 1
-                man.y -= (man.jumpcount**2) * 0.5 * var
-                man.jumpcount -= 1
+                man.y -= ((man.jumpcount**2) / 5) * var
+                man.jumpcount -= 0.5
 
             else:
                 man.isjump = False
                 man.jumpcount = 10
         clock.tick(fps)
-
 
 
 gameloop()
