d6717f892536e973315f0ee2412d8a6f97e823a9
ROHITHK123 <Kedaram@123>
2020-11-29 16:44:14 +0530

    testing

--- DATABASE/p.py
+++ DATABASE/p.py
@@ -1,6 +1,6 @@
 import sqlite3
 a = sqlite3.connect('users.db')
 my_cursor = a.cursor()
-my_cursor.execute('select email_id from data_input where username = (?)',('rohith',))
+my_cursor.execute('select email_id from data_input where username = (?)',('a',))
 a = my_cursor.fetchall()
 print(a)
