f0b1128c83b87b22743127c2be72b8ec183cbe57
ROHITHK123 <Kedaram@123>
2020-12-07 07:07:24 +0530

    bug fixes and added threading

---
+++ a.py
@@ -0,0 +1,26 @@
+import socket
+import time
+
+TCP_IP = 'localhost'
+TCP_PORT = 9001
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+recived_f = 'imgt_thread'+str(time.time()).split('.')[0]+'.jpeg'
+with open(recived_f, 'wb') as f:
+    print('file opened')
+    while True:
+        #print('receiving data...')
+        data = s.recv(BUFFER_SIZE)
+        print('data=%s', (data))
+        if not data:
+            f.close()
+            print('file close()')
+            break
+        # write data to a file
+        f.write(data)
+
+print('Successfully get the file')
+s.close()
+print('connection closed')
